f00ab7b2f6b1a55696970c694b31b54b18e1f11a
Malthael134 <127251883+Malthael134@users.noreply.github.com>
2024-06-17 20:37:05 +0200

    feat: vercel build adapter

--- app/astro.config.mjs
+++ app/astro.config.mjs
@@ -2,18 +2,17 @@ import { defineConfig } from 'astro/config';
 import node from "@astrojs/node";
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
     output: "server",
-    adapter: node({
-        mode: "standalone"
-    }),
+    adapter: vercel(),
     security: {
-        checkOrigin: true,
+        checkOrigin: true
     },
     integrations: [tailwind(), react()],
     experimental: {
-        actions: true,
-    },
+        actions: true
+    }
 });
