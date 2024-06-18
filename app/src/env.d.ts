8cc8221482169f60dd50c3980af40d8696271074
Malthael134 <127251883+Malthael134@users.noreply.github.com>
2024-06-18 13:54:51 +0200

    no t3

--- app/src/env.d.ts
+++
@@ -1,8 +0,0 @@
-/// <reference path="../.astro/actions.d.ts" />
-/// <reference types="astro/client" />
-declare namespace App {
-	interface Locals {
-		session: import("lucia").Session | null;
-		user: import("lucia").User | null;
-	}
-}
