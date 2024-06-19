2c72fee1d61e79bbcfef232f1b949b3976894621
Malthael134 <127251883+Malthael134@users.noreply.github.com>
2024-06-19 12:13:06 +0200

    too stupid for templates :( lol

--- app/src/app/page.tsx
+++ app/src/app/page.tsx
@@ -1,31 +1,8 @@
-"use client"
-import { getServerAuthSession } from "@/server/auth";
-import { SessionProvider, signIn, signOut, useSession } from "next-auth/react";
+import Image from "next/image";
 
-export default function HomePage() {
-
+export default function Home() {
     return (
         <main>
-            <div>
-                <AccountAuthMenu />
-            </div>
-            Hello, T3
         </main>
     );
-}
-
-function AccountAuthMenu() {
-    const { data } = useSession();
-
-    return data ? (
-        <>
-            Signed in as {data.user.email} <br />
-            <button onClick={() => signOut()} className="hover:bg-gray-100">Sign out</button>
-        </>
-    ) : (
-        <>
-            Not signed in <br />
-            <button onClick={() => signIn()} className="hover:bg-gray-100">Sign in</button>
-        </>
-    )
 }
