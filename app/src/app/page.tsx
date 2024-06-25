8cdffb2e31cdff7d5c6c570f9bfe6c541d1a4760
Malthael134 <127251883+Malthael134@users.noreply.github.com>
2024-06-25 13:12:49 +0200

    next ahhh

---
+++ app/src/app/page.tsx
@@ -0,0 +1,10 @@
+import { api } from "@/trpc/server";
+
+export default async function Home() {
+  const hello = await api.post.hello({ text: "from tRPC" });
+
+  return (
+    <main>
+    </main>
+  );
+}
